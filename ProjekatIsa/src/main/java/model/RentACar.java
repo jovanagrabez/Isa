84d7683e847baf7707e20b049d6662baff9b4fbf
Sara Celik <sara.celik@live.com>
2018-11-20 21:01:28 +0100

    rentacar

--- ProjekatIsa/src/main/java/model/RentACar.java
+++ ProjekatIsa/src/main/java/model/RentACar.java
@@ -1,5 +1,80 @@
 package model;
 
-public class RentACar {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "rentacar")
+public class RentACar implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "rentacar_id", nullable = false, updatable = false)
+    private Long id;
+	
+	@Column(name = "name", nullable = false, unique = true, columnDefinition="VARCHAR(40)")
+    private String name;
+	
+	@Column(name = "adress", nullable = false, columnDefinition="VARCHAR(100)")
+    private String adress;
+	
+	@Column(name = "description", nullable = false, columnDefinition="VARCHAR(50)")
+    private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public RentACar(Long id, String name, String adress, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.adress = adress;
+		this.description = description;
+	}
+
+	public RentACar() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
