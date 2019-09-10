dcdc6066bcba7c054ac457c766bc9a6fa7ef93fb
Jovana Grabez <jovanagrabez@uns.ac.rs>
2019-09-10 18:38:00 +0200

    Data.sql

--- ProjekatIsa/src/main/resources/data.sql
+++ ProjekatIsa/src/main/resources/data.sql
@@ -82,21 +82,18 @@ insert into destination(name, country, description) values ('New Delhi', 'Indija
 
 
 
-insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified) 
-	values (1,'Sara','Celik','isasaracelik@gmail.com','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true);
-insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified) values (2,'Admin','Admin','admin@gmail.com',
-	'$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true);
-insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified) values (3,'Admin','Admin','avioadmin@gmail.com',
-	'$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true);
-insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified) values (4,'Admin','Admin','hoteladmin@gmail.com',
-	'$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true);
-insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified) values (5,'Admin','Admin','caradmin@gmail.com',
-	'$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true);
-insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified) values (6,'Admin','Admin','tanja@gmail.com',
-	'$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,false);
-insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified)
-	values (7,'Jovana','Grabez','jovana.grabez@gmail.com','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true);
-
+insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified,points,first_login) 
+	values (1,'Sara','Celik','isasaracelik@gmail.com','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true,15,true);
+insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified,first_login) values (2,'Admin','Admin','admin@gmail.com',
+	'$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true,true);
+insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified,first_login) values (3,'Admin','Admin','avioadmin@gmail.com',
+	'$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true,true);
+insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified,first_login) values (4,'Admin','Admin','hoteladmin@gmail.com',
+	'$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true,true);
+insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified,first_login) values (5,'Admin','Admin','caradmin@gmail.com',
+	'$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true,true);
+insert into user (user_id,first_name,last_name,email,password_hash,enabled,verified,points,first_login) values (6,'Admin','Admin','tanja@gmail.com',
+	'$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra',true,true,16,false);
 
 insert into role (id,name) values (1,'USER');
 insert into role (id,name) values (2,'SYSTEM_ADMIN');
